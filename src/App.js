9f8c5d4faebb420d2f02017aa536015d476e07d0
Shreshth Sharma <shreshthsharma.contact@gmail.com>
2022-09-27 19:49:51 +0530

    background changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+// import './App.css';
+// import TicTacToe from './TicTacToe';
+// function App() {
+//   return (
+//     <>
+//     <TicTacToe/>
+//     </>
+//   );
+// }
+// export default App;
+
+import React from 'react'
+import ParticlesBg from 'particles-bg'
 import './App.css';
 import TicTacToe from './TicTacToe';
-function App() {
-  return (
-    <>
-    <TicTacToe/>
-    </>
-  );
-}
-
-export default App;
+const App=()=> {
+    return (
+      <>
+        <div>
+          <TicTacToe/>
+        </div>
+        <ParticlesBg color="#1087e7" type="cobweb" bg={true} />
+      </>
+    )
+  }
+  export default App;
